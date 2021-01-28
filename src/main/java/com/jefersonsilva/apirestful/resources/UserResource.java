5edf3b29ab576cf97e80c72182137f856093fe9e
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-01-28 15:15:26 -0400

    Inserção de usuário com POST

--- src/main/java/com/jefersonsilva/apirestful/resources/UserResource.java
+++ src/main/java/com/jefersonsilva/apirestful/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.jefersonsilva.apirestful.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jefersonsilva.apirestful.domain.User;
 import com.jefersonsilva.apirestful.dto.UserDTO;
@@ -20,18 +23,26 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET) // == @GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // == @GetMapping
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@RequestMapping(method = RequestMethod.POST) // == @PostMapping
+	public ResponseEntity<Void> findById(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
