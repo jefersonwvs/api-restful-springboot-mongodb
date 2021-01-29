1cc553c2c0ffb51b49f801f186f8bafa29f55454
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-01-29 11:29:41 -0400

    Acrescentando comentários aos posts

--- src/main/java/com/jefersonsilva/apirestful/config/Instantiation.java
+++ src/main/java/com/jefersonsilva/apirestful/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.jefersonsilva.apirestful.domain.Post;
 import com.jefersonsilva.apirestful.domain.User;
 import com.jefersonsilva.apirestful.dto.AuthorDTO;
+import com.jefersonsilva.apirestful.dto.CommentDTO;
 import com.jefersonsilva.apirestful.repository.PostRepository;
 import com.jefersonsilva.apirestful.repository.UserRepository;
 
@@ -30,14 +31,22 @@ public class Instantiation implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();	
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		postRepository.deleteAll();		
+			
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem, amigo!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().add(comment3);
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
