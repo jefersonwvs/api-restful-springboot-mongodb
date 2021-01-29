79d982992f5494c56a1db8ea1f07e4b530b5aa87
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-01-29 14:18:53 -0400

    Consulta simples com @Query

--- src/main/java/com/jefersonsilva/apirestful/repository/PostRepository.java
+++ src/main/java/com/jefersonsilva/apirestful/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.jefersonsilva.apirestful.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jefersonsilva.apirestful.domain.Post;
@@ -11,5 +12,8 @@ import com.jefersonsilva.apirestful.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle2(String text);
 	
 }
